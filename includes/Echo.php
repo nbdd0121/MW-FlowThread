608658ce2cc92f53bbe98f535a5501dc30bb637f
Gary Guo <nbdd0121@hotmail.com>
2016-01-15 01:53:35 +0000

    Fix typo

--- includes/Echo.php
+++ includes/Echo.php
@@ -167,8 +167,8 @@ class EchoHook {
 		return true;
 	}
 
-	public static function onFlowThreadDeleted($post, \User $initiater) {
-		if ($post->userid === 0 || $post->userid === $initiater->getId()) {
+	public static function onFlowThreadDeleted($post, \User $initiator) {
+		if ($post->userid === 0 || $post->userid === $initiator->getId()) {
 			return true;
 		}
 
@@ -187,8 +187,8 @@ class EchoHook {
 		return true;
 	}
 
-	public static function onFlowThreadRecovered($post, \User $initiater) {
-		if ($post->userid === 0 || $post->userid === $initiater->getId()) {
+	public static function onFlowThreadRecovered($post, \User $initiator) {
+		if ($post->userid === 0 || $post->userid === $initiator->getId()) {
 			return true;
 		}
 
