7c497e299c1bdfa23716f5c97f1563db8e924d23
Gary Guo <nbdd0121@hotmail.com>
2016-06-04 01:33:08 +0100

    Bump version

--- FlowThread.alias.php
+++ FlowThread.alias.php
@@ -5,10 +5,12 @@ $specialPageAliases['en'] = array(
 	'FlowThreadImport' => array('FlowThreadImport'),
 	'FlowThreadExport' => array('FlowThreadExport'),
 	'FlowThreadManage' => array('FlowThreadManage'),
+	'FlowThreadLink' => array('FlowThreadLink'),
 );
 
 $specialPageAliases['zh-hans'] = array(
 	'FlowThreadImport' => array('导入FlowThread评论'),
 	'FlowThreadExport' => array('导出FlowThread评论'),
 	'FlowThreadManage' => array('管理FlowThread评论'),
+	'FlowThreadLink' => array('FlowThreadLink'),
 );
