f14500a91223dd9b15120ee30fd8b2cd0b488e2f
Gary Guo <gary@garyguo.net>
2019-05-20 19:33:26 +0100

    Try to improve visual on mobile (#30)

--- assets/flowthread.js
+++ assets/flowthread.js
@@ -148,12 +148,12 @@ Paginator.prototype.repaint = function() {
 
 var pager = new Paginator();
 
-$('#bodyContent').after('<div class="comment-container-top" disabled></div>', '<div class="comment-container"></div>', pager.object, function () {
+$('#bodyContent').after($('<div class="post-content"></div>').append('<div class="comment-container-top" disabled></div>', '<div class="comment-container"></div>', pager.object, function () {
   if (canpost) return createReplyBox(null);
   var noticeContainer = $('<div>').addClass('comment-bannotice');
   noticeContainer.html(config.CantPostNotice);
   return noticeContainer;
-}());
+}()));
 
 if (mw.util.getParamValue('flowthread-page')) {
   reloadComments((parseInt(mw.util.getParamValue('flowthread-page')) - 1) * 10);
